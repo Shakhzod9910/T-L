cfeeb4b55d6417b6f1e95982d35a9d638b432b55
Shakhzodbek <92552024+Shakhzod9910@users.noreply.github.com>
2022-04-14 13:27:40 +0500

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,12 +22,12 @@ useEffect(()=>{
   return (
     <section className="header" id="home">
       <div className="header-wrapper container">
-          <div className="left-side">
+          <div className="left-side" data-aos="fade-right">
               <h1>Smart Fleet Managament Solutions</h1>
               <p>The easiest way to use ELD device and application</p>
               <button onClick={() => scrollToFunc('request')}>Get Started</button>
           </div>
-          <div className="right-side">
+          <div className="right-side" data-aos="fade-left">
             <div className="img-wrapper">
               <Image src={devices} alt="img" />
             </div>
